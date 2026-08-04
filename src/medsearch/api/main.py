7e46f4e3b303fe23c566c64ce35f89817c55b79e
cu6j0n006 <cubahirojoel@gmail.com>
2026-08-04 17:18:42 +0200

    api: boostraping api

---
+++ src/medsearch/api/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/search?q={query}")
+def search_query(query: str | None = None): 
+    return {
+        "query": query
+    } 
---
+++ src/medsearch/api/pubmed_api_client.py
@@ -0,0 +1,6 @@
+import os 
+class PubMedApiClient: 
+    api_key = os.environ("pub_med_API_key")
+    def __init__(self, api_key:str):
+        self.api_key = api_key
+        ...
