275c0477f5236396c5214890af682263d3591c7f
cu6j0n006 <cubahirojoel@gmail.com>
2026-08-05 17:36:44 +0200

    initial API calls

--- src/medsearch/api/main.py
+++ src/medsearch/api/main.py
@@ -2,8 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/search?q={query}")
-def search_query(query: str | None = None): 
-    return {
-        "query": query
-    } 
+def search_query(query: str | None = None):
+    return {"query": query}
--- src/medsearch/api/pubmed_api_client.py
+++ src/medsearch/api/pubmed_api_client.py
@@ -1,6 +1,8 @@
-import os 
-class PubMedApiClient: 
+import os
+
+
+class PubMedApiClient:
     api_key = os.environ("pub_med_API_key")
-    def __init__(self, api_key:str):
+
+    def __init__(self, api_key: str):
         self.api_key = api_key
-        ...
